61c8fb7bf65c888cc894d4394568a28625757937
Caba Beni <caba.beniamin@gmail.com>
2023-10-30 18:40:35 +0200

    Initial commit

---
+++ src/Curs5Homework.java
@@ -0,0 +1,18 @@
+public class Curs5Homework {
+    /*
+        1) scrieti o functie care primeste un sir de numere intregi si returneaza suma lor
+
+        2) scrieti o functie care primeste un sir de numere intregi si returneaza numarul de elemente impare
+
+        3) scrieti o functie care primeste un sir de numere intregi si un alt numar intreg. Returnati toate numerele mai mari decat numarul primit
+
+        4) scrieti o functie care primeste un numar intreg reprezentand targetul de donatii. Donatiile se vor face cu ajutorul obiectului Random. Primim donatii pana cand ajungem la suma dorita. Cand ajungem la suma dorita afisam un mesaj de succes.
+
+        5) rescrieti functia de la 4 cu urmatoarea modificare: functia va primi si un numar maxim de donatii. Cand acesta se termina, campania se va incheia
+
+        6) scrieti o functie care primeste un string cu o fraza (mai multe propozitii despartite prin punct). Printati fiecare propozitie pe o linie noua
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
